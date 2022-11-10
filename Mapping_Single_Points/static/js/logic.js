01666adf85206c75bc2c36b8b8205e7b2e84f691
katiarp <katia.paredes84@gmail.com>
2022-11-09 18:54:06 -0800

    Modified logic.js file

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -6,7 +6,7 @@ console.log("working");
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -15,3 +15,23 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
 // Then we add our 'graymap' tile layer to the map
 
 streets.addTo(map);
+
+//Add a marker to the map for LA, CA
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// let marker = L.circle([34.0522, -118.2437], {
+//     radius: 100
+// }).addTo(map);
+
+// var circle = L.circle([34.0522, -118.2437], {
+//     color: 'black',
+//     fillColor: 'yellow',
+//     fillOpacity: 0.15,
+//     radius: 300
+// }).addTo(map);
+
+let marker= L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    color: "black",
+    fillColor: '#ffffa1'
+}).addTo(map);
