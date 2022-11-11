1c37b19d8d8a5387513609bb0309f7a180eab2d5
katiarp <katia.paredes84@gmail.com>
2022-11-11 14:36:17 -0800

    Changed Map color

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -6,7 +6,7 @@ console.log("working");
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -45,10 +45,7 @@ let cityData = cities;
   cityData.forEach(function(city){
     console.log(city)
     L.circleMarker(city.location, {
-      radius:city.population/200000,
-      color: 'orange',
-      fillColor: 'orange',
-      fillOpacity: 0.15,
+      radius:city.population/100000
     }).addTo(map)
     .bindPopup("<h2>"+ city.city+ ", "+ city.state+ "</h2><hr><h3>Population " + city.population.toLocaleString() + "</h3>")
     .addTo(map);
