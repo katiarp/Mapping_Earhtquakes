dd6c10ab9b492386d1f797b4193427df11993517
katiarp <katia.paredes84@gmail.com>
2022-11-12 18:42:23 -0800

    Created map with Linestrings

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // We create the tile layer that will be the default background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let satellite = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -16,15 +16,15 @@ let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{
 
 // Create a base layer that holds both maps
 let baseMaps = {
-    Street: streets,
+    Satellite: satellite,
     Dark: dark
 };
 
 // Create the map object with the center, zoom level and default layer
 let map = L.map('mapid',{
-    center: [30, 30],
+    center: [44.0, -80.0],
     zoom: 2,
-    layers:[streets]
+    layers:[dark]
 });
 
 //Pass our map layer into our layers control and add the layers control to the map
@@ -32,18 +32,24 @@ L.control.layers(baseMaps).addTo(map);
 
 // Accessing the airport GeoJSON URL
 
-let airportData = "https://raw.githubusercontent.com/katiarp/Mapping_Earhtquakes/Mapping_GeoJSON_Points/majorAirports.json";
+let torontoData = "https://raw.githubusercontent.com/katiarp/Mapping_Earhtquakes/Mapping_GeoJSON_Linestrings/torontoRoutes.json";
 
+//Create a style for the lines
+let myStyle ={ 
+    color:'#ff9900',
+    weight: 2
+}
 
 // Grabing our GeoJSON data
-d3.json(airportData).then(function(data){
+d3.json(torontoData).then(function(data){
     console.log(data); 
      // Creating a GeoJSON layer with the retrieved data.
-    L.geoJSON(data, {
-        onEachFeature: function(feature, layer) {
-            console.log(layer);
-            layer.bindPopup("<h2>" + "Airport code: " + feature.properties.faa + "</h2><hr><h3>" + "Airport name: " + feature.properties.name + "</h3>");
-        }}).addTo(map);
+    L.geoJSON(data,{
+    style: myStyle,
+    onEachFeature: function(feature, layer){
+        layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: " +feature.properties.dst +"</h3>")
+    }
+    }).addTo(map);
 });
 
 
